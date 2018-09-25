3fd37746c662141a2b270cc3b0021995baeae5ae
Robert Bevec <robert.bevec@ijs.si>
2018-09-25 14:11:59 +0000

    function for transforming Matlab point cloud to instance for recognition

---
+++ pc2vox.m
@@ -0,0 +1,34 @@
+function [ Volume ] = pc2vox( ptCloud, VolumeSize, pad_size )
+%PC2VOX Summary of this function goes here
+%   Detailed explanation goes here
+
+
+%This is equivalent to auto mode
+%Find the maximum dimension of the cloud for normalization
+% Generally we would have to calculate the max and min of each axis, but
+% these are already given in the Limits
+largestDistance = [...
+    ptCloud.XLimits(2)-ptCloud.XLimits(1),...
+    ptCloud.YLimits(2)-ptCloud.YLimits(1),...
+    ptCloud.ZLimits(2)-ptCloud.ZLimits(1)];
+
+%largest distance must fit in VolumeSize (which in an integer)
+normalizationFactor = (VolumeSize-1) / max(largestDistance);
+
+centerOffset =  largestDistance/2 + [ptCloud.XLimits(1),ptCloud.YLimits(1),ptCloud.ZLimits(1)];
+
+normalizedCenteredLocation = (ptCloud.Location - centerOffset) .* normalizationFactor + [VolumeSize/2 VolumeSize/2 VolumeSize/2];
+
+instance = zeros(VolumeSize, VolumeSize, VolumeSize);
+
+for i=1:size(normalizedCenteredLocation,1)
+    coordinates = normalizedCenteredLocation(i,:);
+    coordinates = ceil(coordinates);
+    instance(coordinates(1), coordinates(2), coordinates(3)) = 1;
+end
+
+instance = padarray(instance, [pad_size, pad_size, pad_size]);
+Volume = int8(instance);
+
+end
+
